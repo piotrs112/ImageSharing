efb0d440d8200d0a0a256257623e38333a011302
piotrs112 <piotrsocha01@gmail.com>
2021-09-26 21:13:56 +0200

    Cleanup

--- images/signals.py
+++ images/signals.py
@@ -1,9 +1,11 @@
 import os
-from django.db.models.signals import post_delete, post_save, pre_save
+
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from PIL import Image as PIL_Image
 
 from images.models import Image, ImageHeight, UserPlan
+
 
 @receiver(post_save, sender=Image)
 def create_thumbnails(sender, instance, **kwargs):
@@ -11,7 +13,7 @@ def create_thumbnails(sender, instance, **kwargs):
     Create appropriate thumbnails when image is saved
     """
     image = PIL_Image.open(instance.image.name)
-    
+
     user_plan = UserPlan.objects.get(user=instance.owner)
     image_heights = ImageHeight.objects.filter(plan=user_plan.plan)
 
@@ -20,12 +22,6 @@ def create_thumbnails(sender, instance, **kwargs):
         _image = image.resize((int(image.width*height/image.height), height))
         _image.save(f"uploads/{height}/{instance}")
 
-@receiver(post_delete, sender=Image)
-def delete_thumbnails(sender, instance, **kwargs):
-    """
-    Delete thumbnails when original file is deleted
-    """
-    #todo
 
 @receiver(pre_save, sender=ImageHeight)
 def create_image_size_folder(sender, instance, **kwargs):
