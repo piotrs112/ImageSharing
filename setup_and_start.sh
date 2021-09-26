6314c9865c8f71535bd2752b63d0e0057d1863c2
piotrs112 <piotrsocha01@gmail.com>
2021-09-26 21:56:29 +0200

    make resolution folders

--- setup_and_start.sh
+++ setup_and_start.sh
@@ -14,6 +14,8 @@ python3 manage.py migrate
 
 # Create uploads folder
 mkdir uploads
+mkdir uploads/200
+mkdir uploads/400
 
 # Create superuser
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('test_user', '', 'test_password')" |   python3 manage.py shell_plus
